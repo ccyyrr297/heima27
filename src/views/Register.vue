1e3edaad8a06ee6f55e2be5e389c2abfdcb785e7
墨尘 <389079622@qq.com>
2020-08-28 15:49:31 +0800

    配置适配基础样式

---
+++ src/views/Register.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hm-register">
+    我是注册组件
+    <span class="iconfont iconweixin"></span>
+    <span class="iconfont icondianzan"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    console.log('哈哈哈')
+  }
+}
+</script>
+
+<style></style>
